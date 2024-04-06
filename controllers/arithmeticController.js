421162604b5914709fee3ee31089e9a266bceae5
SARATH K <142706927+sarathk73@users.noreply.github.com>
2024-04-06 12:21:37 +0530

    Added Number Validation

--- controllers/arithmeticController.js
+++ controllers/arithmeticController.js
@@ -1,24 +1,36 @@
 // Define the functions for arithmetic operations
 exports.add = (req, res) => {
     const { number1, number2 } = req.body;
+    if (isNaN(number1) || isNaN(number2)) {
+      return res.status(400).send({ error: 'Invalid input numbers' });
+    }
     const result = number1 + number2;
     res.json({ result  });
   };
   
   exports.subtract = (req, res) => {
     const { number1, number2 } = req.body;
+    if (isNaN(number1) || isNaN(number2)) {
+      return res.status(400).send({ error: 'Invalid input numbers' });
+    }
     const result = number1 - number2;
     res.json({ result });
   };
   
   exports.multiply = (req, res) => {
     const { number1, number2 } = req.body;
+    if (isNaN(number1) || isNaN(number2)) {
+      return res.status(400).send({ error: 'Invalid input numbers' });
+    }
     const result = number1 * number2;
     res.json({ result });
   };
   
   exports.divide = (req, res) => {
     const { number1, number2 } = req.body;
+    if (isNaN(number1) || isNaN(number2)) {
+      return res.status(400).send({ error: 'Invalid input numbers' });
+    }
     if (number2 === 0) {
       return res.status(400).json({ error: "Cannot divide by zero." });
     } else {
