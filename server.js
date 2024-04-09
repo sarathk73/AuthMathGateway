08a70c6f375ef81bdc5a850d121957228b480028
SARATH K <142706927+sarathk73@users.noreply.github.com>
2024-04-09 09:01:18 +0530

    Add rate limiting to authentication routes to prevent brute force attacks

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { handleSyntaxError, handleValidationError, handleGenericError } = require('./middlewares/errorHandlers');
 require('dotenv').config();
+const authLimiter = require('./middlewares/rateLimit');
 const app = express();
 const indexRoutes = require('./routes/index');
 const authRoutes = require('./routes/authRoutes'); 
@@ -13,6 +14,7 @@ app.use(handleValidationError);
 
 app.use(handleGenericError);
 
+app.use('/api/auth', authLimiter);
 
 app.use('/api', indexRoutes);
 app.use('/api/auth', authRoutes); 
