38f12f45361900ac1d04bf5f3731fe0122c21b39
SARATH K <142706927+sarathk73@users.noreply.github.com>
2024-04-09 08:50:40 +0530

    Update validation middleware to handle both value and error in arithmetic operations

--- controllers/arithmeticController.js
+++ controllers/arithmeticController.js
@@ -1,11 +1,17 @@
+const Joi = require('joi');
+
 const { arithmeticOperationSchema } = require('../validations/validationSchemas');
 
 const validateArithmeticOperation = (req, res, next) => {
-  const { error } = arithmeticOperationSchema.validate(req.body);
+  const { value, error } = arithmeticOperationSchema.validate(req.body);
   if (error) {
-    return res.status(400).json({ error: error.details[0].message });
+    const err = new Error('Validation failed');
+    err.error = error;
+    err.status = 400;
+    return next(err); 
   }
-  next();
+  req.body = value; 
+  next(); 
 };
 
 exports.add = [validateArithmeticOperation, (req, res) => {
