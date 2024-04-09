768c9c2a4c839ce665a7db8f0f2bf025ef0395d3
SARATH K <142706927+sarathk73@users.noreply.github.com>
2024-04-09 13:46:33 +0530

    Add Swagger JSDoc comments  for authentication endpoint

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,7 +8,38 @@ const users = [
   { id: 1, username: 'user1', password: 'pass1' },
   
 ];
-
+/**
+ * @swagger
+ * /auth/authenticate:
+ *   post:
+ *     summary: Authenticate a user and provide a JWT token
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *             required:
+ *               - username
+ *               - password
+ *     responses:
+ *       200:
+ *         description: Authentication successful, a JWT token is provided
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 accessToken:
+ *                   type: string
+ *       401:
+ *         description: Authentication failed due to invalid credentials
+ */
 router.post('/authenticate', (req, res) => {
  
   const { username, password } = req.body;
